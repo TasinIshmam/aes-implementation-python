ce4da916040a55f6f1e9b4b217eb7d4995955848
Tasin Ishmam <tasinishmam@gmail.com>
2021-03-12 19:29:45 +0600

    Finish upto g_function

---
+++ keyutils.py
@@ -0,0 +1,71 @@
+
+from BitVector import *
+from constants import Constants
+import copy
+
+class Key:
+
+    KEY_EXPANSIONS = 10  # 11 sets of keys including original.
+
+    def __init__(self, key_string):
+        self.key_string = key_string
+        self.key_int_array = Key.generate_key_from_string(key_string)  # Array of ints. Eg - [75, 22..]
+        self.expanded_key_int_array = [[self.key_int_array]]  # array of arrays.
+
+        root_word = self.key_int_array[12:16]
+        Key.g_function_on_root_word(root_word, 1)
+
+    @staticmethod
+    def g_function_on_root_word(root_word, round_no):
+        if not (1 <= round_no <= 10):
+            raise Exception("Invalid round used for generate_round_key")
+
+        # circular byte left shift
+        shifted_root_word = Key.circular_byte_left_shift(root_word)
+        # byte substitution
+        byte_substituted_root_word = Key.byte_substitution_sbox(shifted_root_word)
+        # adding round constant
+        round_constant_int = Constants.round_constants[round_no]
+        round_constant_bitvector = BitVector(intVal=round_constant_int, size=8)
+        root_word_significant_byte_bitvector = BitVector(intVal=byte_substituted_root_word[0], size=8)
+        updated_root_word_significant_byte_bitvector = root_word_significant_byte_bitvector.__xor__(round_constant_bitvector)
+
+        byte_substituted_root_word[0] = updated_root_word_significant_byte_bitvector.intValue()
+
+        return byte_substituted_root_word
+
+
+
+
+
+
+
+    @staticmethod
+    def circular_byte_left_shift(root_word_int_array):
+        return [root_word_int_array[1], root_word_int_array[2], root_word_int_array[3], root_word_int_array[0]]
+
+    @staticmethod
+    def byte_substitution_sbox(root_word_int_array):
+        substituted_array = []
+        for x in range(4):
+            substituted_int = Constants.sbox[root_word_int_array[x]]
+            substituted_array.append(substituted_int)
+        return substituted_array
+
+
+    @staticmethod
+    def generate_key_from_string(key_string):
+        size_adjusted_string = key_string
+        if len(key_string) > 16:
+            size_adjusted_string = key_string[0:16]
+        elif len(key_string) < 16:
+            size_adjusted_string = key_string.ljust(16,'\0')    # pad string to the right with 0's
+
+        key = [ord(size_adjusted_string[x]) for x in range(16) ]
+        print(f'Original Key: {size_adjusted_string}\nKey in Int: {key}')
+        return key
+
+
+# print(BitVector(intVal=0x34, size=8).get_bitvector_in_hex())
+
+Key("Thats my Kung Fu")
